1f02fc2fc98b8a31f85530a7427fe4c83040e05a
Sung Min Shin <ssmptrshin@berkeley.edu>
2019-10-26 13:50:56 -0700

    Created basic layout, started implementation of blocks library

--- app/src/main/java/com/example/forthekidz/MainActivity.java
+++ app/src/main/java/com/example/forthekidz/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.forthekidz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.LinearLayout;
 import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 }
