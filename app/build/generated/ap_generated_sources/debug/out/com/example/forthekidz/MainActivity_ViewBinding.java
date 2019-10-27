9eaf3e90fe6f8397a6cd7cd22103009c638c6ba7
Nathan Chou <nathanchou4@gmail.com>
2019-10-26 19:01:35 -0700

    Nathan push

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/forthekidz/MainActivity_ViewBinding.java
@@ -0,0 +1,60 @@
+// Generated code from Butter Knife. Do not modify!
+package com.example.forthekidz;
+
+import android.view.View;
+import androidx.annotation.CallSuper;
+import androidx.annotation.UiThread;
+import butterknife.Unbinder;
+import butterknife.internal.DebouncingOnClickListener;
+import butterknife.internal.Utils;
+import java.lang.IllegalStateException;
+import java.lang.Override;
+
+public class MainActivity_ViewBinding implements Unbinder {
+  private MainActivity target;
+
+  private View view7f0700ad;
+
+  private View view7f07003f;
+
+  @UiThread
+  public MainActivity_ViewBinding(MainActivity target) {
+    this(target, target.getWindow().getDecorView());
+  }
+
+  @UiThread
+  public MainActivity_ViewBinding(final MainActivity target, View source) {
+    this.target = target;
+
+    View view;
+    view = Utils.findRequiredView(source, R.id.testButton, "method 'showModal'");
+    view7f0700ad = view;
+    view.setOnClickListener(new DebouncingOnClickListener() {
+      @Override
+      public void doClick(View p0) {
+        target.showModal(p0);
+      }
+    });
+    view = Utils.findRequiredView(source, R.id.closeModalButton, "method 'closeModal'");
+    view7f07003f = view;
+    view.setOnClickListener(new DebouncingOnClickListener() {
+      @Override
+      public void doClick(View p0) {
+        target.closeModal(p0);
+      }
+    });
+  }
+
+  @Override
+  @CallSuper
+  public void unbind() {
+    if (target == null) throw new IllegalStateException("Bindings already cleared.");
+    target = null;
+
+
+    view7f0700ad.setOnClickListener(null);
+    view7f0700ad = null;
+    view7f07003f.setOnClickListener(null);
+    view7f07003f = null;
+  }
+}
--- app/src/main/java/com/example/forthekidz/MainActivity.java
+++ app/src/main/java/com/example/forthekidz/MainActivity.java
@@ -2,22 +2,41 @@ package com.example.forthekidz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+
+import com.google.blockly.model.Block;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
+    Dialog myDialog;
+    BlockService blockService = new BlockService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDialog = new Dialog(this);
+//        System.out.println("HELLO HELLO HELLO");
+        System.out.println(blockService.getBlockDefinitionsJsonPaths().get(0));
     }
 
+    @OnClick(R.id.testButton)
+    public void showModal(View v) {
+        myDialog.setContentView(R.layout.test_modal);
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
 
-    @OnClick(R.id.testButton) void onClick() {
-
+    @OnClick(R.id.closeModalButton)
+    public void closeModal(View v) {
+        myDialog.dismiss();
     }
 }
